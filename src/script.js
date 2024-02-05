7316d9c38019d013533e7651c877290b55fc37c6
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-02-05 17:56:16 +0200

    Update logic game

--- src/script.js
+++ src/script.js
@@ -1,1 +1,30 @@
+const btnRollDiceRefs = document.querySelector('.btn--roll');
+const diceImageRefs = document.querySelector('.dice');
 
+let countPlayer = 0;
+let currentPlayerOneValue = 0;
+
+btnRollDiceRefs.addEventListener('click', onBtnRollDiceClick);
+
+function onBtnRollDiceClick() {
+  const randomNumber = Math.floor(Math.random() * 6) + 1;
+  diceMarkup(randomNumber);
+  checkRandomNumber(randomNumber);
+  // currentPlayerValue += randomNumber;
+  // document.querySelector(`#current--${countPlayer}`).textContent =
+  //   currentPlayerValue;
+}
+
+function diceMarkup(number) {
+  diceImageRefs.textContent = number;
+  diceImageRefs.classList.remove('hidden');
+}
+
+function checkRandomNumber(number) {
+  if (number === 1) {
+    countPlayer = 1;
+  }
+  currentPlayerValue += number;
+  document.querySelector(`#current--${countPlayer}`).textContent =
+    currentPlayerValue;
+}
