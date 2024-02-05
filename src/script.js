7859b7bf223789b0f1a10cb8c72a66e457a59d26
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-02-05 21:56:26 +0200

    Complete game

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const btnRollDiceRefs = document.querySelector('.btn--roll');
 const diceImageRefs = document.querySelector('.dice');
 const holdBtnRefs = document.querySelector('.btn--hold');
+const btnNewGame = document.querySelector('.btn--new');
 
 let countPlayer = 0;
 let currentPlayerOneValue = 0;
@@ -9,6 +10,9 @@ let totalValuePlayerTwo = 0;
 
 btnRollDiceRefs.addEventListener('click', onBtnRollDiceClick);
 holdBtnRefs.addEventListener('click', onBtnHoldClick);
+btnNewGame.addEventListener('click', onClickNewGame);
+
+styleCheck(countPlayer);
 
 function onBtnRollDiceClick() {
   const randomNumber = Math.floor(Math.random() * 6) + 1;
@@ -20,20 +24,44 @@ function onBtnHoldClick() {
   if (countPlayer === 0) {
     totalValuePlayerOne += currentPlayerOneValue;
     document.querySelector(`#score--0`).textContent = totalValuePlayerOne;
+    resetCurrentValue('#current--0', 1);
+    styleCheck();
+    finalGame();
   } else {
     totalValuePlayerTwo += currentPlayerOneValue;
     document.querySelector(`#score--1`).textContent = totalValuePlayerTwo;
+    resetCurrentValue('#current--1', 0);
+    styleCheck();
+    finalGame();
   }
-
-  document.querySelector(`#current--${countPlayer}`).textContent = 0;
-  countPlayer === 1 ? (countPlayer = 0) : (countPlayer = 1);
   currentPlayerOneValue = 0;
+}
+
+function onClickNewGame() {
+  countPlayer = 0;
+  currentPlayerOneValue = 0;
+  totalValuePlayerOne = 0;
+  totalValuePlayerTwo = 0;
+  document.querySelector(`#score--0`).textContent = 0;
+  document.querySelector(`#score--1`).textContent = 0;
+  diceImageRefs.textContent = 0;
+  document.querySelector('.player--0').classList.remove('player--winner');
+  document.querySelector('.player--1').classList.remove('player--winner');
+  document.querySelector(`.player--0`).classList.add('player--active');
+  holdBtnRefs.disabled = false;
+  btnRollDiceRefs.disabled = false;
+  diceImageRefs.style.display = 'block';
 }
 
 function checkRandomNumber(number) {
   if (number === 1) {
-    document.querySelector(`#current--${countPlayer}`).textContent = 0;
-    countPlayer === 1 ? (countPlayer = 0) : (countPlayer = 1);
+    if (countPlayer === 0) {
+      resetCurrentValue('#current--0', 1);
+      styleCheck();
+    } else {
+      resetCurrentValue('#current--1', 0);
+      styleCheck();
+    }
     currentPlayerOneValue = number - 1;
   }
   currentPlayerOneValue += number;
@@ -44,4 +72,36 @@ function checkRandomNumber(number) {
 function diceMarkup(number) {
   diceImageRefs.textContent = number;
   diceImageRefs.classList.remove('hidden');
+}
+
+function resetCurrentValue(className, countValue) {
+  document.querySelector(className).textContent = 0;
+  countPlayer = countValue;
+}
+
+function styleCheck() {
+  if (countPlayer === 0) {
+    document.querySelector(`.player--0`).classList.add('player--active');
+    document.querySelector('.player--1').classList.remove('player--active');
+  } else {
+    document.querySelector(`.player--1`).classList.add('player--active');
+    document.querySelector('.player--0').classList.remove('player--active');
+  }
+}
+
+function finalGame() {
+  if (totalValuePlayerOne >= 100) {
+    finalStyle('.player--0', true, 'none');
+  }
+
+  if (totalValuePlayerTwo >= 100) {
+    finalStyle('.player--1', true, 'none');
+  }
+}
+
+function finalStyle(namePlayer, btnStatus, diceStyle) {
+  document.querySelector(namePlayer).classList.add('player--winner');
+  holdBtnRefs.disabled = btnStatus;
+  btnRollDiceRefs.disabled = btnStatus;
+  diceImageRefs.style.display = diceStyle;
 }
